709b798a6c0f5bbbd68a3e7c4927ac1382a0e161
Michael Hernandez <michael.hernandez@bobcats.gcsu.edu>
2024-08-25 18:05:56 -0400

    Initial commit

---
+++ hernandez.py
@@ -0,0 +1,8 @@
+# create a hello world application
+print("Hello, World!")
+
+# make a new line asking a user for their name
+name = input("What is your name? ")
+
+# print the name
+print("Hello, " + name + "!")
